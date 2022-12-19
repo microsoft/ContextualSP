48f8ed479a4d2ef0a6f5c65da9c91f38ac559007
SivilTaram <qianlxc@outlook.com>
2022-12-19 12:13:41 +0800

    Use fewer epochs to train the model

--- awakening_latent_grounding/train_spider_ground.sh
+++ awakening_latent_grounding/train_spider_ground.sh
@@ -1,6 +1,6 @@
 python train.py -model SpiderAlignmentModel -bert bert-large-uncased-whole-word-masking \
     -lr 5e-5 -train_bs 6 \
-    -acc_steps 4 -alw linear_20-50 -num_epochs 50  \
+    -acc_steps 4 -alw linear_20-30 -num_epochs 30  \
     --data_dir data/spider_grounding \
     --out_dir checkpoints/model_spider \
     --warmup_steps 2000
--- awakening_latent_grounding/train_wtq_ground.sh
+++ awakening_latent_grounding/train_wtq_ground.sh
@@ -1,4 +1,4 @@
 python train.py -model WTQAlignmentModel -bert bert-base-uncased \
-    -lr 3e-5 -train_bs 16 -alw linear_5-10 -num_epochs 50  \
+    -lr 3e-5 -train_bs 16 -alw linear_5-10 -num_epochs 20  \
     --data_dir data/wtq_grounding \
     --out_dir checkpoints/model_wtq
